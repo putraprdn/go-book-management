07dac0dd845d12b4e1a4027b7174ebe00edae2c0
putraprdn <80741806+putraprdn@users.noreply.github.com>
2024-03-25 14:15:47 +0700

    first commit

---
+++ cmd/main/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/putraprdn/go-book-management/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookstoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
+}
